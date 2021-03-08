2a451b39ccdb6bce2817c9732ebb4d6a090e0ebe
JeremyRivera <jeremyrivera23@yahoo.com>
2021-03-08 15:06:23 -0500

    First Layout with scss and variables defining the size for mobile, tablet and desktop

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Tile from "../Tile/index";
+import "./App.scss";
+
+function App() {
+  return <Tile />;
+}
+
+export default App;
---
+++ src/components/Tile/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./Tile.scss";
+
+const Tile = () => {
+    return (
+        <div className="container">
+            <div className="block-1">
+                <div className="box-1">A</div>
+                <div className="box-2">B</div>
+                <div className="box-3">C</div>
+            </div>
+            <div className="block-2">
+                <div className="box-4">D</div>
+                <div className="box-5">E</div>
+                <div className="box-6">F</div>
+            </div>
+            <div className="block-3">
+                <div className="box-7">G</div>
+                <div className="box-8">H</div>
+                <div className="box-9">I</div>
+            </div>
+        </div>
+    )
+};
+
+export default Tile;
